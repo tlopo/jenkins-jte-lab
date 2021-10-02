c941e06f2749f6ec7e4d244025da588d3ca82ca2
Tiago Lopo da Silva <tiago@NBC02C76H5MD6T.EMIRATES.COM>
2021-10-02 14:14:47 +0400

    testing

--- libraries/tlopo/steps/foo.groovy
+++ libraries/tlopo/steps/foo.groovy
@@ -1,25 +1,36 @@
-def generateStage(job) {
+/*def generateStage(job) {
     return {
         stage("stage: ${job}") {
             echo "This is ${job}."
         }
     }
-}
+}*/
 
 void call(var){
-
+/*
     def jobs = ["JobA", "JobB", "JobC"]
      
     def parallelStagesMap = jobs.collectEntries {
         ["${it}" : generateStage(it)]
     }
-     
+*/     
 
 
     stage('parallel stage') {
         steps {
             script {
-                parallel parallelStagesMap
+                parallel {
+                    stage('JobA') {
+                        steps {
+                            echo 'JobA'
+                        }
+                    }
+                    stage('JobB') {
+                        steps {
+                            echo 'JobB'
+                        }
+                    }
+                }
             }
         }
     }
