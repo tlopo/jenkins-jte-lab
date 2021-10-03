7e7370f5ca520a23991ca1f7e8a5bcb5cdb3139c
Tiago Lopo da Silva <tiago@NBC02C76H5MD6T.EMIRATES.COM>
2021-10-03 09:00:00 +0400

    testing

--- libraries/tlopo/steps/foo.groovy
+++ libraries/tlopo/steps/foo.groovy
@@ -1,11 +1,14 @@
-void call(var){
+def getParallel(var) {
     def p = [:]
-
     var.each {
         p[it] = { echo "${it}" }
     }
+    return p
+}
 
-    parallel p 
+void call(var){
+
+    parallel getParallel(var) 
 
     stage("I am foo, received ${var}") {
         echo "I am foo and received ${var}"
